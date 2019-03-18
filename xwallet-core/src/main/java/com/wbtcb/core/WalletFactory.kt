0b2f44fe6355bdb813b41f3cd603addf0d67c4d9
Tomáš Smolík <tomas.smolik@einvent.eu>
2019-03-18 14:55:48 +0100

    WalletFactory - create by class name

--- xwallet-core/src/main/java/com/wbtcb/core/WalletFactory.kt
+++ xwallet-core/src/main/java/com/wbtcb/core/WalletFactory.kt
@@ -1,6 +1,21 @@
 package com.wbtcb.core
 
+import java.lang.IllegalArgumentException
+
 object WalletFactory {
+
+    @Suppress("UNCHECKED_CAST")
+    fun createWallet(walletClassName: String, walletSpecification: WalletSpecification? = null): Wallet {
+        try {
+            val walletProviderClass = Class.forName(walletClassName)
+            if (Wallet::class.java.isAssignableFrom(walletProviderClass)) {
+                return createWallet((walletProviderClass as Class<Wallet>), walletSpecification)
+            }
+            throw IllegalArgumentException("Class '$walletClassName' does not implement Wallet")
+        } catch (e: ClassNotFoundException) {
+            throw IllegalArgumentException("Problem creating Wallet (class not found)", e)
+        }
+    }
 
     fun <W : Wallet> createWallet(walletClass: Class<W>, walletSpecification: WalletSpecification? = null): W {
         return createInstance(walletClass).apply {
