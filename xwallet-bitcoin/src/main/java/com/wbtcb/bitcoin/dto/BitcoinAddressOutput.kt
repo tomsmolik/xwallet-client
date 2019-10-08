aaa5a650394a89342bb70cbab619751c472127dc
Lubos Lacina <lacom@email.cz>
2019-10-08 15:55:32 +0200

    Fix Invalid parameter, key-value pair must contain exactly one key

--- xwallet-bitcoin/src/main/java/com/wbtcb/bitcoin/dto/BitcoinAddressOutput.kt
+++ xwallet-bitcoin/src/main/java/com/wbtcb/bitcoin/dto/BitcoinAddressOutput.kt
@@ -1,13 +1,11 @@
 package com.wbtcb.bitcoin.dto
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.math.BigDecimal
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class BitcoinAddressOutput(
 
-    @get:JsonAnyGetter
     val address: MutableList<HashMap<String, BigDecimal>> = mutableListOf()
 
     /*
